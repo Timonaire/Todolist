d6607092c3c04988849dae064cc638aca63da337
Timonaire <timothyikechukwu94@gmail.com>
2023-01-20 16:02:24 +0100

    first commit

---
+++ mapping.js
@@ -0,0 +1,11 @@
+const companies= [
+    {name: "Company one", category: "finance", start:1981, end:2004},
+    {name: "Company two", category: "retail", start:1992, end:2008},
+    {name: "Company three", category: "auto", start:1999, end:2007},
+    {name: "Company four", category: "retail", start:1989, end:2010},
+    {name: "Company five", category: "tech", start:2009, end:2014},
+    {name: "Company six", category: "finance", start:1987, end:2010},
+    {name: "Company seven", category: "auto", start:1986, end:1996},
+    {name: "Company eight", category: "tech", start:2011, end:2016},
+    {name: "Company nine", category: "retail", start:1981, end:1989}
+]
